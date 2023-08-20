9d569943c10331e8d13ba3fed0652750e24044c1
shitiandazhi <135299404+shitiandazhi@users.noreply.github.com>
2023-08-20 09:33:45 +0000

    fix admin/grant

--- app/controllers/admin/grant_comments_controller.rb
+++ app/controllers/admin/grant_comments_controller.rb
@@ -1,2 +1,13 @@
 class Admin::GrantCommentsController < ApplicationController
+
+   def destroy
+    GrantComment.find(params[:id]).destroy
+    redirect_to admin_grant_path(params[:grant_id])
+   end
+
+  private
+
+  def grant_comment_params
+    params.require(:grant_comment).permit(:comment)
+  end
 end
--- app/controllers/admin/grants_controller.rb
+++ app/controllers/admin/grants_controller.rb
@@ -1,2 +1,21 @@
 class Admin::GrantsController < ApplicationController
+
+   def index
+    @grant_comment = GrantComment.all
+    @Tags = Tag.all
+     if
+      params[:tag_id].present?
+      @grants = Grant.where("tag_id LIKE?","%#{params[:tag_id]}%")
+     elsif
+      params[:word].present?
+      @grants = Grant.where('name LIKE ?', "%#{params[:word]}%")
+     else
+      @grants = Grant.all
+    end
+   end
+
+   def show
+    @grant = Grant.find(params[:id])
+    @grant_comment = GrantComment.all
+   end
 end
