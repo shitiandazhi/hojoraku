edc8e1c2d4c170facaf2d5337b3efe342e9be086
shitiandazhi <135299404+shitiandazhi@users.noreply.github.com>
2023-09-01 09:26:57 +0000

    fix db

--- db/migrate/20230830105722_add_star_to_grants.rb
+++
@@ -1,5 +0,0 @@
-class AddStarToGrants < ActiveRecord::Migration[6.1]
-  def change
-    add_column :grants, :star, :string
-  end
-end
--- db/migrate/20230901073419_add_tag_id_to_grants.rb
+++
@@ -1,5 +0,0 @@
-class AddTagIdToGrants < ActiveRecord::Migration[6.1]
-  def change
-    add_column :grants, :tag_id, :integer
-  end
-end
