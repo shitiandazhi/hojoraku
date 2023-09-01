5e2650af058460a32ec892649ff7f99bbc878367
shitiandazhi <135299404+shitiandazhi@users.noreply.github.com>
2023-09-01 14:39:10 +0000

    fix db:grant

--- app/models/grant.rb
+++ app/models/grant.rb
@@ -1,4 +1,5 @@
 class Grant < ApplicationRecord
+
 belongs_to :user
 belongs_to :tag
 
--- db/migrate/20230901142327_create_grants.rb
+++ db/migrate/20230901142327_create_grants.rb
@@ -2,6 +2,7 @@ class CreateGrants < ActiveRecord::Migration[6.1]
   def change
     create_table :grants do |t|
       t.integer :user_id
+      t.integer :tag_id
       t.string :name
       t.string :background
       t.string :body
