8477f8cc1e1545375c8505b1306fa3426d5b9b99
shitiandazhi <135299404+shitiandazhi@users.noreply.github.com>
2023-09-15 07:25:27 +0000

    add admin/grant/show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+def score_color_class(score)
+  case score
+  when -Float::INFINITY...-0.8
+    "red-score"
+  when -0.7...0.1
+    "orange-score"
+  when 0.0...0.7
+    "green-score"
+  when 0.8..Float::INFINITY
+    "blue-score"
+  else
+    "default-score" # スコアが範囲外の場合のデフォルトスタイル
+  end
+end
 end
