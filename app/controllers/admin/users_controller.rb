5617737c0107f2320d9ae49c9db7e15453c6f4fd
shitiandazhi <135299404+shitiandazhi@users.noreply.github.com>
2023-09-18 09:35:37 +0000

    fix grant/top

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -14,9 +14,13 @@ class Admin::UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to admin_user_path
+  @user = User.find(params[:id])
+
+  if @user.update(user_params)
+    redirect_to admin_root_path
+  else
+    render :edit
+  end
   end
 
   private
